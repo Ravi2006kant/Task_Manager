4c22dbe7ff1f64ffc08925239f64c2672a35a209
Ravi <ravi5gangster@gmail.com>
2026-05-01 16:52:13 +0530

    half done textfield

--- lib/add_list.dart
+++ lib/add_list.dart
@@ -14,20 +14,62 @@ class _AddListState extends State<AddList> {
 
         appBar: AppBar(
           backgroundColor: Colors.pink,
-          title: Text("Add Task",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w900)),
+          title: Center(child: Text("Add Task",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w900,color: Colors.white))),
           iconTheme:IconThemeData(color: Colors.white), //change the arrow color
           
         ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-             TextField(), 
-             TextField(),
-             DatePickerDialog(firstDate: DateTime(DateTime.april), lastDate: DateTime(DateTime.friday)),
-             ElevatedButton(onPressed: (){}, child: Text("Submit"))
-          ],
-        ),
+      body: 
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 20),
+              child: Card(
+                elevation: 2,
+                child: Column(
+                  children: [
+                    TextField(
+                      cursorColor: Colors.pink,
+                       keyboardType: TextInputType.name,
+                       minLines: 1,
+
+                       
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide: BorderSide(color: Colors.pink)
+                        )
+                      ),
+                    ),
+
+
+SizedBox(height: 50,),
+
+
+                    TextField(maxLines: 3,),
+
+  SizedBox(height: 50,),
+                  
+                    OutlinedButton(onPressed: () async {
+                         DateTime? dateTime = await showDatePicker(context: context ,
+                          firstDate: DateTime(2020), lastDate: DateTime(2025));
+                    }, child: Text("Choose Date & Time")),
+
+                   SizedBox(height: 50,),
+                    ElevatedButton(onPressed: (){
+                      
+                      Navigator.push(context,MaterialPageRoute(builder: (context) => AddList()));
+                    }, child: Text("Submit"))
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
+      
     );
     
   }
--- lib/task_list.dart
+++ lib/task_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/add_list.dart';
 
 class TaskList extends StatefulWidget{
   const TaskList({super.key});
@@ -11,13 +12,28 @@ class _TaskList extends State<TaskList>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(title: Center(child: Text("Task List",style: TextStyle(fontSize: 25,
-      color: Colors.white,fontWeight: FontWeight.w900),)),backgroundColor: Colors.pink,),
+      color: Colors.white,fontWeight: FontWeight.w800),)),backgroundColor: Colors.pink,),
       body: Column(
         children: [
-          Card(
-            child: Row(children: [Text("data")],),
+          Center(
+            child: Card(
+              child: Column(
+                children: [
+                  TextField(),
+                  TextField(maxLines: 3,),
+                  OutlinedButton(onPressed: () async {
+                       DateTime? dateTime = await showDatePicker(context: context ,
+                        firstDate: DateTime(2020), lastDate: DateTime(2025));
+                  }, child: Text("Choose Date & Time")),
+                 
+                  ElevatedButton(onPressed: (){
+                    
+                    Navigator.push(context,MaterialPageRoute(builder: (context) => AddList()));
+                  }, child: Text("Submit"))
+                ],
+              ),
+            ),
           )
         ],
       ),
